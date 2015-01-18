06f8b719e0e26bcc8a06aea3c4ba8cff03d92856
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-18 16:14:01 +0100

    Test runner skeleton (From Carlos Ble https://bitbucket.org/carlosble/javascript4testers)

---
+++ src/meetingtools.js
@@ -0,0 +1,4 @@
+
+function dummy() {
+	return 2
+}
---
+++ src/unit_tests.js
@@ -0,0 +1,8 @@
+describe("the behavior of the code in the examples", function(){
+
+  it("calculates the average of an array", function(){
+      expect(dummy()).toEqual(2);      
+  });
+
+});
+

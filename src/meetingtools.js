6ee99e08c3c241186965a09f1859ab4eb6699f19
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-20 15:25:52 +0100

    Initial functions

--- src/meetingtools.js
+++ src/meetingtools.js
@@ -1,9 +1,34 @@
 var meetingtools = {};
 
+
+
+
 (function(ns){
-	ns.dummy = function dummy() {
-		return 2
-	}
+	function shuffle(o) {
+		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+			return o;
+	};
+	
+	ns.createDailyMeeting = function (members, initialTime){
+		var members = members;
+		var initialTime = initialTime;
+		var orderedMembers;
+		var remainingTime;
+
+      	return {
+      		startMeeting: function(){
+      			orderedMembers = shuffle(members);
+      			remainingTime = initialTime;
+      			console.log(orderedMembers);
+      		},
+        	interventionOrder: function(){
+            	return orderedMembers;
+          	},
+          	remainingTime: function(){
+          		return remainingTime;
+          	},
+        }
+    };
 
 }(meetingtools));
 
--- src/unit_tests.js
+++ src/unit_tests.js
@@ -1,7 +1,14 @@
-describe("the behavior of the code in the examples", function(){
+describe("Meeting tools", function(){
 
-  it("calculates the average of an array", function(){
-      expect(meetingtools.dummy()).toEqual(2);      
+  it("calculates intervention order for the daily", function(){
+  		daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+  		daily.startMeeting();
+      	expect(daily.interventionOrder().length).toEqual(3);      
+  });
+  it("initialy all time remaining", function(){
+  		daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+  		daily.startMeeting();
+      	expect(daily.remainingTime()).toEqual(10*60);
   });
 
 });

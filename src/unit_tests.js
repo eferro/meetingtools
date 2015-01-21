4754037747701e0d4f7ad47eba34d92b41934505
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-21 09:55:11 +0100

    Reformat

--- src/unit_tests.js
+++ src/unit_tests.js
@@ -1,15 +1,15 @@
 describe("Meeting tools", function(){
 
-  it("calculates intervention order for the daily", function(){
-  		daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
-  		daily.startMeeting();
-      	expect(daily.interventionOrder().length).toEqual(3);      
-  });
-  it("initialy all time remaining", function(){
-  		daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
-  		daily.startMeeting();
-      	expect(daily.remainingTime()).toEqual(10*60);
-  });
+    it("calculates intervention order for the daily", function(){
+        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+        daily.startMeeting();
+        expect(daily.interventionOrder().length).toEqual(3);      
+    });
+    it("initialy all time remaining", function(){
+        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+        daily.startMeeting();
+        expect(daily.remainingTime()).toEqual(10*60);
+    });
 
 });
 

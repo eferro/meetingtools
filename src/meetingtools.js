2e127eb080908876acac4b10e11728371ad845c6
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-21 10:21:58 +0100

    Added a clock

--- src/meetingtools.js
+++ src/meetingtools.js
@@ -6,21 +6,26 @@ var meetingtools = {};
 		return o;
 	};
 
-	ns.createDailyMeeting = function (members, initialDuration){
+	ns.createDailyMeeting = function (members, initialDuration, clock){
 		var members = members;
 		var initialDuration = initialDuration;
+		var initialTime;
+		var clock = clock;
 		var orderedMembers;
 
 		return {
 			startMeeting: function(){
 				//console.log(Math.floor((new Date).getTime()/1000));		
 				orderedMembers = shuffle(members);
+				initialTime = clock.getSeconds();
 			},
 			interventionOrder: function(){
 				return orderedMembers;
 			},
 			remainingTime: function(){
-				return initialDuration;
+				var actualTime = clock.getSeconds();
+				var consumedTime = actualTime - initialTime;
+				return initialDuration - consumedTime;
 			},
 		}
 	};
--- src/unit_tests.js
+++ src/unit_tests.js
@@ -1,14 +1,30 @@
 describe("Meeting tools", function(){
 
+    clock = {
+        getSeconds: function(){
+            return 0;
+        }
+    };
+
     it("calculates intervention order for the daily", function(){
-        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60, clock);
         daily.startMeeting();
         expect(daily.interventionOrder().length).toEqual(3);      
     });
     it("initialy all time remaining", function(){
-        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60);
+        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60, clock);
         daily.startMeeting();
         expect(daily.remainingTime()).toEqual(10*60);
+    });
+
+    it("when 10 second passes the remainingTime is 9*60", function(){        
+        daily = meetingtools.createDailyMeeting(["user1", "user2", "user3"], 10*60, clock);
+        daily.startMeeting();
+
+        clock.getSeconds = function(){
+            return 60;
+        };
+        expect(daily.remainingTime()).toEqual(10*60-60);
     });
 
 });

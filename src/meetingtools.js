d17ba10d4e905cdc76f32f5748cd1a0e6d8f1fc1
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2015-01-26 19:19:56 +0100

    WIP. redesing

--- src/meetingtools.js
+++ src/meetingtools.js
@@ -4,7 +4,7 @@ var meetingtools = {};
 	function shuffle(o) {
 		for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
 		return o;
-	};
+	}
 
 	ns.createDailyMeeting = function (members, initialDuration, clock){
 		var initialTime, orderedMembers;
